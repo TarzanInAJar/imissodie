fc53c393d5c89074b32cff0b801fcb13adec9b2e
tarzaninajar <rybread@gmail.com>
2018-05-19 15:57:05 -0400

    added 'boring' view of imissodie camera view)

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -1,10 +1,47 @@
 <template>
-    
+    <div>
+
+
+        <div class="row">
+            <pre style="background-color: black">
+
+
+
+
+
+
+
+
+                
+            </pre>
+        </div>
+
+
+        <div class="row">
+            <v-btn color="primary" dark>
+                <v-icon>fa-play</v-icon>
+            </v-btn>
+        </div>
+
+        <div class="row">
+            <v-btn color="primary">
+                <v-icon>fa-coffee</v-icon>
+            </v-btn>
+            <v-btn color="primary">
+                <v-icon>fa-bullhorn</v-icon>
+            </v-btn>
+            <v-btn color="primary">
+                <v-icon>fa-microphone</v-icon>
+            </v-btn>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
     export default {
-        name: "Boring.vue"
+        name: "Boring"
     }
 </script>
 
