af83a6892df4fdcf7c66418f41b9abf765f1c4d6
tarzaninajar <rybread@gmail.com>
2018-05-19 15:56:21 -0400

    removing vue logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,15 @@
 <template>
+  <v-app>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
