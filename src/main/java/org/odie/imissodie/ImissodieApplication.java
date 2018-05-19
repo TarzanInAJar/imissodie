fc3093a0c91c0fd2b6c73342498bdf2aefa6d345
tarzaninajar <rybread@gmail.com>
2018-05-18 23:13:56 -0400

    initial commit - hello world application

---
+++ src/main/java/org/odie/imissodie/ImissodieApplication.java
@@ -0,0 +1,12 @@
+package org.odie.imissodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImissodieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImissodieApplication.class, args);
+    }
+}
