e4991a2b2557dfcecf85543b6939adccd34b5196
Ryan <rybread@gmail.com>
2018-05-26 22:31:21 -0400

    'call' button now works

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -6,35 +6,41 @@
                 <img src="http://www.imissodie.com:8080/stream/video.mjpeg" >
         </div>
 
-<!--
-        <div class="row">
-            <v-btn color="primary" dark>
-                <v-icon>fa-play</v-icon>
-            </v-btn>
-        </div>
--->
         <div class="row">
             <v-btn color="primary">
                 <v-icon>fa-coffee</v-icon>
             </v-btn>
-            <v-btn color="primary">
-                <v-icon>fa-bullhorn</v-icon>
+            <v-btn v-bind:disabled="calling" color="primary" @click="call">
+                <v-icon v-if="calling" class="fa-spin">fa-spinner</v-icon>
+                <v-icon v-else>fa-bullhorn</v-icon>
             </v-btn>
-<!--
-            <v-btn color="primary">
-                <v-icon>fa-microphone</v-icon>
-            </v-btn>
--->
         </div>
-
     </div>
 
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        name: "Boring"
+        name: "Boring",
+        data() {
+            return {
+                calling: false,
+                apiUrl: "http://www.imissodie.com:8090/"
+            }
+        },
+        methods: {
+            call(){
+                this.calling = true;
+                axios.get(this.apiUrl + "/call")
+                    .catch(function(error) {
+                        console.log(error);
+                    })
+                    .finally(() => (this.calling = false));
+            }
+        }
     }
+
 </script>
 
 <style scoped>
