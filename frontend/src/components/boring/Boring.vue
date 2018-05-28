8a090d8267e6162083778442c63953b5e844d24b
Ryan <rybread@gmail.com>
2018-05-28 02:09:10 -0400

    WIP: finally got this fucking thing to work, needs cleaning up. going to bed tho

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -1,13 +1,9 @@
 <template>
     <div>
-
-
-        <div class="row">
-                <img src="http://www.imissodie.com:8080/stream/video.mjpeg" >
-        </div>
+        <video height="300" id="video"></video>
 
         <div class="row">
-            <v-btn color="primary">
+            <v-btn color="primary" @click="video">
                 <v-icon>fa-coffee</v-icon>
             </v-btn>
             <v-btn v-bind:disabled="calling" color="primary" @click="call">
@@ -19,14 +15,19 @@
 
 </template>
 
+
 <script>
     import axios from 'axios';
+    import signal from './signalling';
+
+
     export default {
         name: "Boring",
         data() {
             return {
                 calling: false,
-                apiUrl: "http://www.imissodie.com:8090/"
+                apiUrl: "http://www.imissodie.com:8090/",
+                videoPlaying: false
             }
         },
         methods: {
@@ -37,6 +38,38 @@
                         console.log(error);
                     })
                     .finally(() => (this.calling = false));
+            },
+            video() {
+                var signalObj = signal.signal('wss://192.168.0.106:8443/webrtc',
+                    function (stream) {
+                        var video = document.getElementById('video');
+                        console.log('got a stream!');
+                        video.srcObject = stream;
+                        video.play();
+                    },
+                    function (error) {
+                        alert(error);
+                    },
+                    function () {
+                        console.log('websocket closed. bye bye!');
+                        var video = document.getElementById('video');
+                        video.srcObject = null;
+                        //video.src = ''; // deprecated
+                        //ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        //isStreaming = false;
+                    },
+                    function (message) {
+                        alert(message);
+                    }
+                );
+
+            }
+        },
+        watch: {
+            videoPlaying: function(playing) {
+                if (playing){
+
+                }
             }
         }
     }
