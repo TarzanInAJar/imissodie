be6b78f4959c1e14c99f476476f938c8bfba4d07
tarzaninajar <rybread@gmail.com>
2018-08-09 15:42:26 -0400

    added a toolbar. preparing for a nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <v-app dark>
+    <v-toolbar>
+      <v-toolbar-side-icon @click="menu = !menu"><v-icon>menu</v-icon></v-toolbar-side-icon>
+      <v-toolbar-title>imissodie</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span>menutoggle: {{menu}}</span>
+    </v-toolbar>
+      <!--TODO put in a nav bar-->
       <router-view/>
   </v-app>
 </template>
 
 <script>
   export default {
-    name: 'App'
+    name: 'App',
+    data: function() {
+        return {
+            menu: false
+        }
+    }
   }
 </script>
 
