5ba4ff17b7091c909ea41a1f64603e73c148338e
tarzaninajar <rybread@gmail.com>
2018-08-09 16:25:39 -0400

    WIP: beginning work in converting main view to a v-card

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -1,28 +1,31 @@
 <template>
-    <div>
-        <div class="row">
+    <v-card raised style="min-height: 300px;">
+        <v-card-media >
+            <div class="row">
                 <img src="http://www.imissodie.com:8080/stream/video.mjpeg" >
-        </div>
+            </div>
+        </v-card-media>
 
 
-        <div class="row">
-            <!--<v-btn color="primary" dark>-->
-                <!--<v-icon>fa-play</v-icon>-->
-            <!--</v-btn>-->
-            <v-btn color="primary">
-                <v-icon>fa-coffee</v-icon>
-            </v-btn>
-            <v-btn color="primary">
-                <v-icon>fa-bullhorn</v-icon>
-            </v-btn>
-<!--
-            <v-btn color="primary">
-                <v-icon>fa-microphone</v-icon>
-            </v-btn>
--->
-        </div>
+        <v-card-actions>
 
-    </div>
+          <!--<v-btn color="primary" dark>-->
+          <!--<v-icon>fa-play</v-icon>-->
+          <!--</v-btn>-->
+          <v-btn color="primary">
+            <v-icon>fa-coffee</v-icon>
+          </v-btn>
+          <v-btn color="primary">
+            <v-icon>fa-bullhorn</v-icon>
+          </v-btn>
+          <!--
+                      <v-btn color="primary">
+                          <v-icon>fa-microphone</v-icon>
+                      </v-btn>
+          -->
+        </v-card-actions>
+
+    </v-card>
 
 </template>
 
