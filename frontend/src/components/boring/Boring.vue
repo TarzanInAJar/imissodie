826ef926eb9b402c8ef5ac6aa5a4f307d710ef6c
tarzaninajar <rybread@gmail.com>
2018-08-09 15:20:37 -0400

    moving button

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -6,14 +6,11 @@
                 <img src="http://www.imissodie.com:8080/stream/video.mjpeg" >
         </div>
 
-<!--
+
         <div class="row">
-            <v-btn color="primary" dark>
-                <v-icon>fa-play</v-icon>
-            </v-btn>
-        </div>
--->
-        <div class="row">
+            <!--<v-btn color="primary" dark>-->
+                <!--<v-icon>fa-play</v-icon>-->
+            <!--</v-btn>-->
             <v-btn color="primary">
                 <v-icon>fa-coffee</v-icon>
             </v-btn>
