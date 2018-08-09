6a3fe7e75435aa2e7506a2b1d0db9705b6703121
tarzaninajar <rybread@gmail.com>
2018-08-09 16:47:31 -0400

    reformatting, aligning video/controls to middle

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -1,38 +1,51 @@
 <template>
-    <v-card raised style="min-height: 300px;">
-        <v-card-media >
-            <div class="row">
-                <img src="http://www.imissodie.com:8080/stream/video.mjpeg" >
-            </div>
-        </v-card-media>
+  <v-card class="ma-2"
+    raised
+    style="min-height: 300px;"
+  >
+    <v-card-media>
+      <v-layout>
+        <v-spacer></v-spacer>
+        <v-flex xs10>
+          <img src="http://www.imissodie.com:8080/stream/video.mjpeg">
+        </v-flex>
+        <v-spacer></v-spacer>
+      </v-layout>
+    </v-card-media>
 
-
-        <v-card-actions>
-
-          <!--<v-btn color="primary" dark>-->
-          <!--<v-icon>fa-play</v-icon>-->
-          <!--</v-btn>-->
+    <v-card-actions>
+      <v-layout>
+        <v-spacer></v-spacer>
           <v-btn color="primary">
             <v-icon>fa-coffee</v-icon>
           </v-btn>
           <v-btn color="primary">
             <v-icon>fa-bullhorn</v-icon>
           </v-btn>
-          <!--
-                      <v-btn color="primary">
-                          <v-icon>fa-microphone</v-icon>
-                      </v-btn>
-          -->
-        </v-card-actions>
+        <v-spacer></v-spacer>
 
-    </v-card>
+      <!--
+        <v-btn color="primary" dark>
+          <v-icon>fa-play</v-icon>
+        </v-btn>
+      -->
+
+      <!--
+        <v-btn color="primary">
+          <v-icon>fa-microphone</v-icon>
+        </v-btn>
+      -->
+      </v-layout>
+    </v-card-actions>
+
+  </v-card>
 
 </template>
 
 <script>
-    export default {
-        name: "Boring"
-    }
+  export default {
+    name: "Boring"
+  }
 </script>
 
 <style scoped>
