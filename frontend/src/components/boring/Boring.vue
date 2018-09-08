0ff0f0800ae1d809efbecb8f4dc46c7bcbb7926a
Ryan <rybread@gmail.com>
2018-09-08 16:07:27 -0400

    wip: setting up basic authentication over https. will likely scrap this and let nginx take care of it with a truststore

--- frontend/src/components/boring/Boring.vue
+++ frontend/src/components/boring/Boring.vue
@@ -1,24 +1,26 @@
 <template>
-        <!-- this is for webrtc - replace later -->
-        <!-- <video height="300" id="video"></video> -->
+        <!-- -->
   <v-card class="ma-4"
     raised
     style="min-height: 300px;"
   >
-    <v-card-media @click="simulateConnection" class="ma-2">
-      <v-layout>
-        <v-spacer></v-spacer>
-        <v-flex xs10>
+    <v-card-media @click="videoToggle" class="ma-2">
+      <v-layout row wrap>
+        <v-flex xs12>
           <span v-if="status == 'disconnected'">
-            click here to connect
+            click anywhere to connect
           </span>
           <v-icon v-else-if="status == 'connecting'">fa-spin fa-spinner</v-icon>
-          <img v-else-if="status == 'connected'" src="http://www.imissodie.com:8080/stream/video.mjpeg">
           <span v-else-if="status == 'failed'">
             connection failed! click to try again
           </span>
+          <span v-else-if="status == 'connected'">
+            click anywhere to disconnect
+          </span>
         </v-flex>
-        <v-spacer></v-spacer>
+        <v-flex xs12>
+          <video style="background-color: black;" height="300" id="video"></video>
+        </v-flex>
       </v-layout>
     </v-card-media>
 
@@ -28,7 +30,7 @@
           <v-btn :disabled="status != 'connected'" color="primary">
             <v-icon>fa-coffee</v-icon>
           </v-btn>
-          <v-btn :disabled="status != 'connected'" color="primary">
+          <v-btn @click="call" :disabled="status != 'connected' || calling" color="primary">
             <v-icon>fa-bullhorn</v-icon>
           </v-btn>
 
@@ -64,7 +66,8 @@
                 calling: false,
                 apiUrl: "http://www.imissodie.com:8090/",
                 status: 'disconnected',
-                simulationClickCount: 0
+                simulationClickCount: 0,
+                validPassword: null
             }
         },
         methods: {
@@ -87,41 +90,62 @@
                 if (this.status != 'failed' && ++this.simulationClickCount == 3) {
                     this.simulationClickCount = 0;
                 }
-
             },
+            videoToggle() {
+              if (this.status == "disconnected" || this.status == "failed"){
+                  this.video();
+              } else if (this.status == "connected" || this.status == "connecting"){
+                  signal.hangup();
+              }
+            },
+
             call() {
+                console.log("calling...");
                 this.calling = true;
-                axios.get(this.apiUrl + "/call")
+                axios.get(this.apiUrl + "call")
                     .catch(function (error) {
                         console.log(error);
                     })
                     .finally(() => (this.calling = false));
             },
             video() {
-                var signalObj = signal.signal('wss://192.168.0.106:8443/webrtc',
+                var vm = this;
+                vm.status = "connecting";
+                var password;
+                if (this.validPassword){
+                    password = this.validPassword;
+                } else {
+                    password = window.prompt("Please enter the password");
+                }
+                if (!password){
+                    return;
+                }
+
+                var signalObj = signal.signal('wss://user:' + password + '@192.168.0.106:8443/stream/webrtc',
                     function (stream) {
                         var video = document.getElementById('video');
                         console.log('got a stream!');
+                        vm.validPassword = password;
+                        vm.status = "connected";
                         video.srcObject = stream;
                         video.play();
                     },
                     function (error) {
+                        vm.status = "failed";
                         alert(error);
                     },
                     function () {
                         console.log('websocket closed. bye bye!');
+                        vm.status = "disconnected";
                         var video = document.getElementById('video');
                         video.srcObject = null;
-                        //video.src = ''; // deprecated
-                        //ctx.clearRect(0, 0, canvas.width, canvas.height);
-                        //isStreaming = false;
                     },
                     function (message) {
                         alert(message);
                     }
                 );
-
             }
+
         }
     }
 </script>
